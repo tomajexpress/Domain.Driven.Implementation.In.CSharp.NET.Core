dcdb3458145265ace82252c06258650e61dcae02
a.Toumaj <aman.toumaj@gmail.com>
2021-03-04 19:36:10 +0330

    refactoring

--- EShoppingTutorialWebAPI/Models/OrderModels/OrderItemSaveRequestModel.cs
+++ EShoppingTutorialWebAPI/Models/OrderModels/OrderItemSaveRequestModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using EShoppingTutorial.Core.Domain.ValueObjects;
 
 namespace EShoppingTutorialWebAPI.Models.OrderModels
