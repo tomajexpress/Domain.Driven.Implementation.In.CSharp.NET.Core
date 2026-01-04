6289db0f2be7d75ada659864057fd5a59182fa5c
Aman Toumaj <tomajexpress@gmail.com>
2026-01-05 01:37:33 +0300

    cleaning EShoppingTutorialWebAPI - GlobalUsings

--- EShoppingTutorialWebAPI/GlobalUsings.cs
+++ EShoppingTutorialWebAPI/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿// Frameworks
 global using MediatR;
-global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using FluentValidation;
