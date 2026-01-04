3f2157b1cee669ddd6751b3a053d1f650d49f471
Aman Toumaj <tomajexpress@gmail.com>
2026-01-04 20:39:44 +0300

    removing redundant comments from BaseException class

--- Framework.Core/SharedKernel/Exceptions/BaseException.cs
+++ Framework.Core/SharedKernel/Exceptions/BaseException.cs
@@ -2,7 +2,6 @@
 
 /// <summary>
 /// Base exception for all domain-specific exceptions.
-/// Inheriting from Exception is the modern standard over ApplicationException.
 /// </summary>
 public class BaseException(string? message = null, Exception? innerException = null)
     : Exception(message, innerException);
